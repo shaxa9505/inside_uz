27c4d2337f39fcbba843f289c0d26847ec723cfb
shaxa9505 <shohruxmeliboyev8@gmail.com>
2025-03-25 16:10:11 +0900

    Course pageniyam tugattim

--- routes/admin/courses.js
+++ routes/admin/courses.js
@@ -7,10 +7,15 @@ const router = Router();
 router.get("/courses", async (req, res) => {
 
   const courses = await Courses.find({});
+  console.log(courses);
 
   res.render("admin/courses", {
     title: "Курсы",
-    courses
+    courses,
+    editSuccess: req.flash("editSuccess"),
+    deleteSuccess: req.flash("deleteSuccess"),
+    successCourse: req.flash("successCourse"),
+    
   })
 
 })
@@ -28,12 +33,50 @@ router.get("/courseAdd", (req, res) => {
 router.post("/courseAdd", async (req, res) => {
 
   const course = await Courses.create(req.body)
-  console.log(course);
+  // console.log(course);
+  req.flash("successCourse", "")
   res.redirect("/admin")
+})
 
+router.get("/courseEdit/:id", async (req, res) => {
+  const courses = await Courses.findById(req.params.id);
+  console.log(courses)
+  res.render("admin/courseEdit", {
+    title: "Изменить",
+    courses,
+    errorCourses: req.flash("errorCourses")
+  })
 })
 
 
+router.post("/courseEdit/:id", async (req, res) => {
+  
+  const { course, time, weekdays, topic } = req.body;
+  const courses = await Courses.find({});
+
+  // if(!courses) {
+  //   req.flash("errorLink", `К сожалению, такой страницы не найдено ${fullUrl}`)
+  //   res.redirect("/admin")
+  //   return
+  // }
+
+  if(!course || !time || !weekdays || !topic) {
+    req.flash("errorCourses", "Заполните всю таблицу")
+    res.redirect(`/admin/courseEdit/${req.params.id}`)
+    return
+  }
+
+  await Courses.findByIdAndUpdate(req.params.id, {course, time, weekdays, topic});
+  req.flash("editSuccess", "Вы успешно изменили")
+  res.redirect("/admin/courses")
+
+})
+
+router.get("/courseDelete/:id", async (req, res) => {
+  await Courses.findByIdAndDelete(req.params.id);
+  req.flash("deleteSuccess", "Вы успешно удалили!");
+  res.redirect("/admin/courses")  
+})
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const Students = require("../models/Students")
+const Courses = require("../models/Courses")
 
 
 router.get('/', async (req, res) => {
 
   const students = await Students.find({});
+  const courses = await Courses.find({});
 
-  res.render('index', { title: 'Insideuz',  students});
+  res.render('index', { title: 'Insideuz',  students, courses});
 });
 
 module.exports = router;
